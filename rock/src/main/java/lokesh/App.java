7282227eb3fd8b96bfa9f8dc442c96e6206c2dcc
Lokesh-Patnam <157806988+Lokesh-Patnam@users.noreply.github.com>
2024-04-27 20:06:49 +0530

    Update App.java

--- rock/src/main/java/lokesh/App.java
+++ rock/src/main/java/lokesh/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+System.out.println( "How are you" );
+System.out.println( "What are you doing" );
+System.out.println( "How the day goes" );
     }
 }
