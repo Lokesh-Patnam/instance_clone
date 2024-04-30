2185df83876c954c1bb76d03c777fac2d8000a93
Lokesh-Patnam <157806988+Lokesh-Patnam@users.noreply.github.com>
2024-04-30 12:02:00 +0530

    Update App.java

--- rajath/src/main/java/lokesh/App.java
+++ rajath/src/main/java/lokesh/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+System.out.println( "Hello rajath" );
+System.out.println( "Hello lokesh" );
+System.out.println( "bye" );
     }
 }
